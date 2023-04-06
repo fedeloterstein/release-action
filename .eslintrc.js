7358889c5755dde7f2ec1e7c521517ed8efa474a
fedeloterstein <federicoloterstein@gmail.com>
2023-04-06 15:59:09 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.export = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    root: true,
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  }
